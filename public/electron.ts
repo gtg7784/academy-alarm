186a0e67f5d6df910a62b41c5cd83296c399f9a1
gtg7784 <gtg7784@naver.com>
2020-05-23 15:15:49 +0900

    add electron setting at public dir

---
+++ public/electron.ts
@@ -0,0 +1,43 @@
+import { app, BrowserWindow } from 'electron';
+import * as isDev from 'electron-is-dev';
+import * as path from 'path';
+
+let mainWindow: BrowserWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    alwaysOnTop: true,
+    center: true,
+    //fullscreen: true,
+    kiosk: !isDev,
+    resizable: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  if(isDev) {
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+  }
+  
+  mainWindow.on('closed', () => {
+    mainWindow = undefined!;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
